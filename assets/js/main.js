b8ad9f368a343041aa845ca7cb586b8ada8efb1e
BeastTheNinja <134609528+BeastTheNinja@users.noreply.github.com>
2025-05-14 08:22:17 +0200

    update to form and footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,9 +30,6 @@ prevBtn.addEventListener("click", () => {
 });
 // -----------------------------------------------------------------------------------------------------------
 document.getElementById('firstname').addEventListener('input', validateFirstName);
-document.getElementById('lastname').addEventListener('input', validateLastName);
-document.getElementById('address').addEventListener('input', validateAddress);
-document.getElementById('zipcode').addEventListener('input', validateZipcode);
 document.getElementById('email').addEventListener('input', validateEmail);
 
 function validateFirstName() {
@@ -42,26 +39,6 @@ function validateFirstName() {
     }
 }
 
-function validateLastName() {
-    const lastName = document.getElementById('lastname').value;
-    if (lastName.length < 2) {
-        // alert('Efternavn skal være mindst 2 karakterer');
-    }
-}
-
-function validateAddress() {
-    const address = document.getElementById('address').value;
-    if (address.length < 5) {
-        // alert('Adresse skal være mindst 5 karakterer');
-    }
-}
-
-function validateZipcode() {
-    const zipcode = document.getElementById('zipcode').value;
-    if (!/^\d+$/.test(zipcode)) {
-        // alert('Postnummer skal kun indeholde tal');
-    }
-}
 
 function validateEmail() {
     const email = document.getElementById('email').value;
@@ -79,12 +56,9 @@ document.getElementById('myForm').addEventListener('submit', function(e) {
 
 function validateForm() {
     const firstName = document.getElementById('firstname').value;
-    const lastName = document.getElementById('lastname').value;
-    const address = document.getElementById('address').value;
-    const zipcode = document.getElementById('zipcode').value;
     const email = document.getElementById('email').value;
 
-    if (firstName.length >= 2 && lastName.length >= 2 && address.length >= 5 && /^\d+$/.test(zipcode) && /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)) {
+    if (firstName.length >= 2 && /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)) {
         alert('Formularen er valideret korrekt');
         // Eventuelt sende formularen eller noget andet
     } else {
